f3d9b8bfc4ec8232c1476bae02358f8e215ac9ec
Lee Gauthier <lgauthie@uvic.ca>
2014-03-26 13:50:41 -0700

    Lighting workin!

--- ray_tracer.py
+++ ray_tracer.py
@@ -6,24 +6,35 @@ import png
 import math as m
 import numpy as np
 
+class Material(object):
+    def __init__(self, color, trans, refl, shin,
+                 spec=np.array([1,1,1]), diff=np.array([0.6,0.6,0.6])):
+        self.color = color
+        self.trans = trans
+        self.reflectivity = refl
+        self.shininess = shin
+        self.specular = spec
+        self.diffuse = diff
+        self.ambient = np.array([0.2,0.2,0.2])
+
 class World(object):
-    def __init__(self, width, height, fov, entities):
+    # Have Camera
+    def __init__(self, width, height, fov, entities, lights):
         self.width = width
         self.height = height
         self.fov = float(fov)
         self.aspectratio = self.width/float(self.height)
         self.angle = m.tan(m.pi * 0.5 * self.fov / 180.);
         self.entities = entities
-        self.max_depth = 5
+        self.max_depth = 6
+        self.lights = lights
 
 class Circle(object):
-    def __init__(self, center, radius, color, trans, refl):
+    def __init__(self, center, radius, material):
         self.center = center
         self.radius = radius
         self.radius2 = radius ** 2
-        self.color = color
-        self.trans = trans
-        self.refl = refl
+        self.material = material
 
     def intersected(self, raydir, rayorig):
         l = self.center - rayorig
@@ -41,11 +52,14 @@ class Circle(object):
         t = tca - thc
         return (True, t)
 
+    def get_normal(self, point):
+        return normalize(point - self.center)
+
 class Plane(object):
-    def __init__(self, normal, d):
-        self.normal = normal
+    def __init__(self, normal, d, material):
+        self.normal = normalize(normal)
         self.d = d
-        self.color = np.array([1.,0.4,1.])
+        self.material = material
 
     def intersected(self, raydir, rayorig):
         den = self.normal.dot(raydir)
@@ -58,27 +72,42 @@ class Plane(object):
                 return (True, t)
         return (False, 0)
 
+    def get_normal(self, _):
+        return self.normal
+
 def main():
-    width = 300
-    height = 200
+    width = 640
+    height = 480
 
     entities = [
-        Circle(np.array([0., -10004., -20.]),
-               10000, np.array([0.2, 0.2, 0.2]), 0, 0.0),
+        Circle(np.array([0., -10003., -20.]),
+               10000, Material(color=np.array([0.2, 0.5, 0.3]),
+                               trans=0, refl=0.1, shin=1)),
         Circle(np.array([0., 0., -20.]),
-               4, np.array([1.00, 0.32, 0.36]), 1, 0.5),
+               4, Material(color=np.array([1.00, 0.32, 0.36]),
+                           trans=1, refl=0.5, shin=16)),
         Circle(np.array([5., -1., -15.]),
-               4, np.array([0.90, 0.76, 0.46]), 1, 0),
+               4, Material(color=np.array([0.90, 0.76, 0.46]),
+                           trans=1, refl=0.6, shin=32)),
         Circle(np.array([5., 0., -25.]),
-               3, np.array([0.65, 0.77, 0.97]), 1, 0.0),
-        Circle(np.array([-5.5, 0, -15]),
-               3, np.array([0.90, 0.90, 0.90]), 1, 0.0),
-        Plane(np.array([-1., 0., 0.]), 3),
+               4, Material(color=np.array([0.65, 0.77, 0.97]),
+                           trans=1, refl=0.2, shin=6)),
+        Circle(np.array([-5.5, 0., -15.]),
+               4, Material(color=np.array([0.90, 0.90, 0.90]),
+                           trans=1, refl=0.1, shin=16)),
+        Plane(np.array([-0.6, 0., 0.]), 3,
+               Material(color=np.array([0.90, 0.20, 0.50]),
+                        trans=1, refl=0.07, shin=6)),
+    ]
+    lights = [
+        np.array([-16.001, 5.001, 15.001])
     ]
 
-    world = World(width, height, 60., entities)
-
-    image = np.array([[trace(x, y, world) for y in range(width)]
+    world = World(width, height, 70., entities, lights)
+    def compute_color(x, y):
+        ray_dir = compute_ray_dir(x, y, world)
+        return np.clip(trace(world, ray_dir) * 255, 0, 255)
+    image = np.array([[compute_color(x, y) for y in range(width)]
                        for x in xrange(height)])
 
     image = image.reshape(height, width*3)
@@ -86,86 +115,74 @@ def main():
         w = png.Writer(width, height)
         w.write(f, image)
 
-def trace(x, y, world, ray_origin=np.array([0,0,0]), depth=0):
-    color = np.array([0,0,0])
-    ray_dir = compute_ray_dir(x, y, world)
-    smallest_dist = sys.maxint
+def trace(world, ray_dir, ray_origin=np.array([0,0,0]), depth=0):
+    color = np.array([0,0,0]) # Default Color to black
+
+    # Find nearest object
+    nearest_dist = sys.maxint
+    nearest_entity = None
     for entity in world.entities:
         (is_intersected, dist) = entity.intersected(ray_dir, ray_origin)
-        if is_intersected and dist < smallest_dist:
-            color = entity.color
-            smallest_dist = dist
+        if is_intersected and dist < nearest_dist:
+            nearest_entity = entity
+            nearest_dist = dist
 
-    #if True: # Ray hits more than one boundry box
-    #    # Find the boundary box near to the camera position
-    #    # so it will find the object near the eye.
-    #    # Get the object inside the nearest boundary
-    #    pass
-    #    if True: # Ray hits the object
-    #        # Get intersection points
-    #        pass
-    #        if True: # More than one intersection
-    #            # Find nearest to camera
-    #            pass
-    #        else:
-    #            # you have two options (choose one):
-    #            #     1: don't color the pixel, the Color remains black
-    #            #     2: The point is our interested point;
-    #            pass
-    #    # now give the intersected point a proper color
-    #    color = np.array([255,0,0]) # Calculate Color
-    return color * 255
+    if not nearest_entity:
+        return color
 
+    intersect = ray_origin + ray_dir * nearest_dist
+    normal = nearest_entity.get_normal(intersect)
 
+    # Shadows
+    light_dir = normalize(world.lights[0] - intersect)
+    inter1 = intersect + (light_dir * 0.1)
+    for entity in world.entities:
+        (is_intersected, dist) = entity.intersected(light_dir, inter1)
+        if is_intersected:
+            color = nearest_entity.material.ambient * nearest_entity.material.color
+            break
+    else:
+        color = compute_light(nearest_entity,
+                              ray_dir, normal, normalize(world.lights[0]))
+
+    if depth <= world.max_depth and nearest_entity.material.reflectivity > 0.05:
+        refl_color = trace(world, reflect(ray_dir, normal), inter1, depth+1)
+        color += nearest_entity.material.reflectivity * refl_color
+
+    return color
+
+def compute_light(entity, ray_dir, normal, light_dir):
+    reflect_dir = reflect(light_dir, normal)
+
+    lambertian = max(light_dir.dot(normal), 0.0);
+    specular = 0.0
+
+    if lambertian > 0.0:
+        spec_angle = reflect_dir.dot(ray_dir)
+        specular = pow(max(spec_angle, 0.0), entity.material.shininess)
+
+    specular_reflection = specular * entity.material.specular
+
+    diffuse_reflection = lambertian * entity.material.diffuse
+
+    return specular_reflection * entity.material.color \
+         + diffuse_reflection * entity.material.color \
+         + entity.material.ambient * entity.material.color
+
+# make this smarter!
 def compute_ray_dir(x, y, world):
-    inv_width = (1/float(world.width))
+    inv_width  = (1/float(world.width))
     inv_height = (1/float(world.height))
     a = (2 * ((x + 0.5) * inv_width) - 1) * world.angle * world.aspectratio
     b = (1 - 2 * ((y + 0.5) * inv_height)) * world.angle
-    # a = m.tan(world.fov/2.)*((x-(world.width/2.))/world.width/2.)
-    # b = m.tan(world.fov/2.)*(((world.height/2.)-y)/world.height/2.)
     v = np.array([a, b, -1.])
     return normalize(v)
-    #return world.eye + (v/ np.sqrt(v.dot(v)))
 
-class Camera(object):
-    def __init__(self, eye, fovx, fovy, width, height):
-        self.fovx = fovx
-        self.fovy = fovy
-        self.width = width
-        self.height = height
+def reflect(I, N):
+    return I - 2.0 * N.dot(I) * N
 
 def normalize(x):
     return x/np.sqrt(x.dot(x))
-
-def look_at(eye, at, up):
-    mat = np.identity(4)
-
-    e = eye
-    a = e - at
-    b = up
-
-    w = normalize(a)
-    u = normalize(np.cross(b, w))
-    v = np.cross(w, u)
-
-    mat[0,0] = u[0]
-    mat[1,0] = u[1]
-    mat[2,0] = u[2]
-
-    mat[0,1] = v[0]
-    mat[1,1] = v[1]
-    mat[2,1] = v[2]
-
-    mat[0,2] = w[0]
-    mat[1,2] = w[1]
-    mat[2,2] = w[2]
-
-    # Set up translation
-    mat[0,3] =  -(u[0] * e[0]) - (u[1] * e[1]) - (u[2] * e[2]);
-    mat[1,3] =  -(v[0] * e[0]) - (v[1] * e[1]) - (v[2] * e[2]);
-    mat[2,3] =  -(w[0] * e[0]) - (w[1] * e[1]) - (w[2] * e[2]);
-    print mat
 
 if __name__=="__main__":
     main()
